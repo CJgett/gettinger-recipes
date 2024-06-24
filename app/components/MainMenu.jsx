84ccfc66ee87b69c006cff642fbc4e598d2cb5d6
Carly Gettinger <cjgettinger@gmail.com>
2024-06-24 15:36:51 +0200

    fix - change class to className

--- app/components/MainMenu.jsx
+++ app/components/MainMenu.jsx
@@ -7,9 +7,9 @@ export default function MainMenu() {
       <h1>A Pinch of 한미</h1>
       <nav>
         <ol>
-          <li class="menu-item"><a href="#">Recipes</a></li>
-          <li class="menu-item"><a href="#">Remixer</a></li>
-          <li class="menu-item"><a href="#">About</a></li>
+          <li className="menu-item"><a href="#">Recipes</a></li>
+          <li className="menu-item"><a href="#">Remixer</a></li>
+          <li className="menu-item"><a href="#">About</a></li>
         </ol>
       </nav>
     </header>
