ccf13be120c786c90157b63b04cf441d6df98e45
Carly Gettinger <cjgettinger@gmail.com>
2024-06-25 20:49:13 +0200

    init dark mode toggle

---
+++ app/components/Toggle.jsx
@@ -0,0 +1,8 @@
+export default function CustomToggle() {
+  return(
+    <label className="custom-toggle">
+      <input type="checkbox"></input>
+      <span className="toggle-button"></span>
+    </label>
+  );
+}
