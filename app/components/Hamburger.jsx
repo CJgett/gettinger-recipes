f6d3428523a4a1f5a7b2c0e8e29f8e06ccf9fc0e
Carly Gettinger <cjgettinger@gmail.com>
2024-07-21 22:17:29 +0200

    add mobile menu

---
+++ app/components/Hamburger.jsx
@@ -0,0 +1,21 @@
+export default function Hamburger() {
+
+  function toggleMobileMenu(e) {
+    const targetClasses = e.target.classList;
+    console.log(e.target);
+    if (targetClasses.contains("hide")) {
+      targetClasses.remove("hide");
+      e.target.nextElementSibling.classList.remove("hide");
+    } else {
+      targetClasses.add("hide");
+      e.target.nextElementSibling.classList.add("hide");
+    } 
+  }
+  return (
+    <div className="hamburger hide" onClick={toggleMobileMenu}>
+        <div className="hamburger-line"></div>
+        <div className="hamburger-line"></div>
+        <div className="hamburger-line"></div>
+      </div>
+  );
+}
