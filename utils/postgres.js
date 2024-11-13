b719b65951d750d86448c64833379e9061fe6ff9
Carly Gettinger <cjgettinger@gmail.com>
2024-11-13 11:13:37 +0100

    test

--- utils/postgres.js
+++ utils/postgres.js
@@ -2,11 +2,11 @@ import { Pool } from "pg";
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  host: process.env.DB_HOST,
+  /*host: process.env.DB_HOST,
   port: "5432",
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_DATABASE,*/
 });
 
 export async function dbFetch(customQueryText, values) {
@@ -14,7 +14,6 @@ export async function dbFetch(customQueryText, values) {
   try {
     const client = await pool.connect();
     const result = await client.query(customQueryText, values);
-    console.log("here's the data you requested: ", result.rows);
     client.release();
     return result.rows;
   } catch (err) {
