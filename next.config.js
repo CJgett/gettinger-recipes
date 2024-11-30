a9a3e6c665731d0c0dffe6512fe5b004d61928d6
Carly Gettinger <cjgettinger@gmail.com>
2024-11-30 18:43:56 +0100

    update csp

--- next.config.js
+++ next.config.js
@@ -9,6 +9,19 @@ const nextConfig = {
             },
         ],
     },
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: "default-src 'self'; connect-src 'self' https://blob.vercel-storage.com; img-src 'self' https://goxwrlx89aaxmikq.public.blob.vercel-storage.com",
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
