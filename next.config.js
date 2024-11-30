de523f3c9bfa7d6bd92fa8324c0389770fb5699f
Carly Gettinger <cjgettinger@gmail.com>
2024-11-30 18:50:55 +0100

    fix next config file

--- next.config.js
+++ next.config.js
@@ -16,8 +16,15 @@ const nextConfig = {
                 headers: [
                     {
                         key: 'Content-Security-Policy',
-                        value: "default-src 'self'; connect-src 'self' https://blob.vercel-storage.com; img-src 'self' https://goxwrlx89aaxmikq.public.blob.vercel-storage.com",
-                    },
+                        value: [
+                            "default-src 'self' https://blob.vercel-storage.com https://vercel.live",
+                            "connect-src 'self' https://blob.vercel-storage.com https://vercel.live",
+                            "img-src 'self' https://goxwrlx89aaxmikq.public.blob.vercel-storage.com",
+                            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://vercel.live",
+                            "script-src-elem 'self' 'unsafe-inline' https://vercel.live",
+                            "style-src 'self' 'unsafe-inline'"
+                        ].join('; ')
+                    }
                 ],
             },
         ];
