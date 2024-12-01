30a090ba71e5123c249ca3c9f0f6aff142bf4001
Carly Gettinger <cjgettinger@gmail.com>
2024-12-01 20:53:13 +0100

    form formatting :)

--- app/components/form-components/SourceField.jsx
+++ app/components/form-components/SourceField.jsx
@@ -1,10 +1,14 @@
 export default function SourceField({sourceKey, defaultSource=""}) {
   return (
     <div className="source">
-      <label htmlFor={`source-link-${sourceKey}`}>Source Link: </label>
-      <input type="url" id={`source-link-${sourceKey}`} name="source_link" defaultValue={defaultSource.source_link} />
-      <label htmlFor={`source-title-${sourceKey}`}>Source Title: </label>
-      <input id={`source-title-${sourceKey}`} name="source_title" defaultValue={defaultSource.source_title} />
+      <div className="source-link-container">
+        <label htmlFor={`source-link-${sourceKey}`}>Source Link: </label>
+        <input type="url" id={`source-link-${sourceKey}`} name="source_link" defaultValue={defaultSource.source_link} />
+      </div>
+      <div className="source-title-container">
+        <label htmlFor={`source-title-${sourceKey}`}>Source Title: </label>
+        <input id={`source-title-${sourceKey}`} name="source_title" defaultValue={defaultSource.source_title} />
+      </div>
     </div>
   );
 }
