ab79244fd058562f1bd8106e7c50c73d838bda90
Carly Gettinger <cjgettinger@gmail.com>
2025-02-03 14:46:40 +0100

    updates, debug post updates

--- app/components/Recipe.jsx
+++ app/components/Recipe.jsx
@@ -2,7 +2,7 @@ import { dbFetch } from '../../utils/postgres.js'
 import RecipeClientChild from './RecipeClientChild.jsx'
 
 export default async function Recipe(recipeID) {
-  let fetchedRecipe = await dbFetch('SELECT * FROM all_recipes WHERE id = $1', [recipeID.recipeID]);
+  let fetchedRecipe = await dbFetch('SELECT * FROM all_recipes WHERE id = $1', [Number(recipeID.recipeID)]);
   fetchedRecipe = fetchedRecipe[0];
 
   return (
--- app/recipes/[slug]/page.jsx
+++ app/recipes/[slug]/page.jsx
@@ -2,8 +2,8 @@ import Recipe from '../../components/Recipe.jsx'
 import '../../styles/recipe.css'
 import { notFound } from 'next/navigation'
 
-export default function RecipePage({ params }) {
-  const { slug } = params;
+export default async function RecipePage({ params }) {
+  const { slug } = await params;
   const id = slug.split('-').pop(); // Get the ID from the end of the slug
 
   if (!id) {
