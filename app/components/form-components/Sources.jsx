2d329df65564595cc84f87c8572433efd44d98d9
Carly Gettinger <cjgettinger@gmail.com>
2025-02-09 15:32:59 +0100

    cleanup

--- app/components/form-components/Sources.jsx
+++ app/components/form-components/Sources.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 import FieldType from '../../constants/FieldType.jsx'
 import SourceField from './SourceField.jsx'
