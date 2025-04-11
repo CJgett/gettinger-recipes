d926e821c7bb17221490c684b1ddbd5d8bafc385
Carly Gettinger <cjgettinger@gmail.com>
2025-04-11 14:41:17 +1000

    switch homepage section order

--- app/page.jsx
+++ app/page.jsx
@@ -28,24 +28,6 @@ export default function HomePage() {
     <div className="home-wrapper">
       <section className="home-content">
         <div className="narrow-section">
-          <h2>Welcome to the Gang!</h2>
-          <ImagePlusText
-            title="A woman in a sun hat stands in a field of flowers with some cut flowers in her hand."
-            imageSrc={`${publicBlobUrl}/home/flowers-ae38DnNuSTPdNrYLIQ4MQ7Km1L4ATe.jpg`}
-            imageLeft={false} 
-            flexText={1} 
-            flexImage={1} 
-            imageWidth={600} 
-            imageHeight={1000} 
-            altText="A woman in a sun hat stands in a field of flowers with some cut flowers in her hand."
-            pText={`As every other person from the US, I like food, and I like lots of it... same as my entire family. I started making food with my mom as a kid, and we still cook together when I visit - our strategy for pies, where I make the crust and she makes the filling, has never once failed us :)
-
-            If you're on this site and you're not part of the family, well, you are now! 
-
-            (PS: pls don't tell Buna* that we put her recipes on the world wide web - if you do tell her, tell her it definitely wasn't Carly, thank you!)
-
-            *(Romanian for "grandma")`} />
-          </div> 
           <h2>Latest Recipes</h2>
           <div className="recipe-cards-wrapper">
             {loading ? [1,2,3].map((key) => <RecipeCardLoading key={key}/>) : latestRecipes.map((recipe) => (
@@ -57,6 +39,26 @@ export default function HomePage() {
             <Link href="/recipes">
               <button className="view-all-recipes-button">View All Recipes</button>
             </Link>
+          </div>
+          <h2>Welcome to the Gang!</h2>
+          <ImagePlusText
+            title="A woman in a sun hat stands in a field of flowers with some cut flowers in her hand."
+            imageSrc={`${publicBlobUrl}/home/flowers-ae38DnNuSTPdNrYLIQ4MQ7Km1L4ATe.jpg`}
+            imageLeft={false} 
+            flexText={1} 
+            flexImage={1} 
+            imageWidth={600} 
+            imageHeight={1000} 
+            altText="A woman in a sun hat stands in a field of flowers with some cut flowers in her hand."
+            pText={`The Gettinger Gang, that is! 
+              
+            As every other person from the US, we like food, and we like lots of it. I started making food with my mom as a kid, and we still cook together when I visit - our strategy for pies, where I make the crust and she makes the filling, has never once failed us :)
+
+            If you're on this site and you're not part of the family, well, you are now! 
+
+            (PS: pls don't tell Buna* that we put her recipes on the world wide web - if you do tell her, tell her it definitely wasn't Carly, thank you!)
+
+            *(Romanian for "grandma")`} />
           </div>
         </section>
       <section className="banner-img">
