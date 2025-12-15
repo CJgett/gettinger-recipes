da37491f5f9875ee66e2f87a71f68f6c11149a95
CJgett <47866775+CJgett@users.noreply.github.com>
2025-12-15 15:47:13 +1100

    delete about section from homepage

--- app/page.jsx
+++ app/page.jsx
@@ -40,26 +40,7 @@ export default function HomePage() {
               <button className="view-all-recipes-button">View All Recipes</button>
             </Link>
           </div>
-          <h2>Welcome to the Gang!</h2>
-          <ImagePlusText
-            title="A woman in a sun hat stands in a field of flowers with some cut flowers in her hand."
-            imageSrc={`${publicBlobUrl}/home/flowers-ae38DnNuSTPdNrYLIQ4MQ7Km1L4ATe.jpg`}
-            imageLeft={false} 
-            flexText={1} 
-            flexImage={1} 
-            imageWidth={600} 
-            imageHeight={1000} 
-            altText="A woman in a sun hat stands in a field of flowers with some cut flowers in her hand."
-            pText={`The Gettinger Gang, that is! 
-              
-            As every other person from the US, we like food, and we like lots of it. I started making food with my mom as a kid, and we still cook together when I visit - our strategy for pies, where I make the crust and she makes the filling, has never once failed us :)
-
-            If you're on this site and you're not part of the family, well, you are now! 
-
-            (PS: pls don't tell Buna* that we put her recipes on the world wide web - if you do tell her, tell her it definitely wasn't Carly, thank you!)
-
-            *(Romanian for "grandma")`} />
-          </div>
+         </div>
         </section>
       <section className="banner-img">
       </section>
